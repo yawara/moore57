e2ad6c40b42a119c283498b935f0f6ebb50c7c97
Yawara ISHIDA <ni732h.sphl@gmail.com>
2026-05-09 18:50:03 +0900

    Reduce AFiber cardinality to matching fixed counts

--- Moore57.lean
+++ Moore57.lean
@@ -125,6 +125,8 @@ import Moore57.AFiberMatchingPermSymmetry
 import Moore57.AFiberFiberUnionCardinality
 import Moore57.AFiberUnionFilterCardinality
 import Moore57.AFiberRotationTransport
+import Moore57.AFiberCoordinateRotation
+import Moore57.AFiberRotationMatchingCardinality
 import Moore57.TraceCoreCharacterBoundary
 import Moore57.RotationOneFixedBoundBoundary
 import Moore57.RotationOneFixedBoundBoundaryNonempty
